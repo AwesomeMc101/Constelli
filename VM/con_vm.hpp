58fb854e852e74a5778bde37d801ab8cab44819f
AwesomeMc101 <43798963+AwesomeMc101@users.noreply.github.com>
2024-02-28 23:26:55 -0600

    Create con_vm.hpp

---
+++ VM/con_vm.hpp
@@ -0,0 +1,11 @@
+#include "include.h"
+
+#include "con_stack.hpp"
+#include "con_math.hpp"
+#include "con_cfunction.hpp"
+#include "con_concat.hpp"
+
+namespace VM {
+
+	void execute(Instruction_Set* in_St, Stack::Stack* heap);
+}
