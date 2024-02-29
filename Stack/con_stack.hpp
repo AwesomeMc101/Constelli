6c3724c351b627dc5e35190752e6a11587718b01
AwesomeMc101 <43798963+AwesomeMc101@users.noreply.github.com>
2024-02-29 12:38:40 -0600

    Update con_stack.hpp

--- Stack/con_stack.hpp
+++ Stack/con_stack.hpp
@@ -6,10 +6,20 @@ namespace Stack
 	class Stack {
 		std::vector<std::string> stack;
 		std::map<std::string, std::string> var_stack;
+		std::map < std::string, std::vector<Instruction_Set*>> jump_stack;
 	public:
 		void push(std::string data);
 		std::string pop();
 		std::string pop_index(unsigned int idx); //pop with size()-idx index 
+		void pop_back();
 		size_t stacksize();
+		void push_var(std::string iden, std::string value);
+		std::string index_var(std::string iden);
+
+		void new_jump(std::string iden);
+		void push_jump(std::string iden, Instruction_Set* in_St);
+		std::vector<Instruction_Set*> return_jump_instructions(std::string iden);
+
+		void dbg_printvars();
 	};
 }
