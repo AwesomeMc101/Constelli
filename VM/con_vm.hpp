68a86977e9664a3bdb31a1fbf1dd993bb421dfdb
AwesomeMc101 <43798963+AwesomeMc101@users.noreply.github.com>
2024-02-29 20:34:42 -0600

    Update con_vm.hpp

--- VM/con_vm.hpp
+++ VM/con_vm.hpp
@@ -4,8 +4,10 @@
 #include "con_math.hpp"
 #include "con_cfunction.hpp"
 #include "con_concat.hpp"
+#include "con_compare.hpp"
 
 namespace VM {
-
+	std::string decode_instruction(INST instruction);
 	void execute(Instruction_Set* in_St, Stack::Stack* heap);
+	
 }
