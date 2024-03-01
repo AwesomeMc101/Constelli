8a1d8872f0a8479d6e3b8042e424d4fc5420899f
AwesomeMc101 <43798963+AwesomeMc101@users.noreply.github.com>
2024-02-29 20:34:00 -0600

    Create con_compare.hpp

---
+++ Compare/con_compare.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "include.h"
+
+namespace Compare
+{
+	std::string comp_strings(std::string str1, std::string str2, INST comptype);
+	std::string comp_integers(std::string str1, std::string str2, INST comptype);
+}
