1cf8cab67d97b958590c5bd6309933b7a17152d8
AwesomeMc101 <43798963+AwesomeMc101@users.noreply.github.com>
2024-03-03 19:20:41 -0600

    Create con_cfunction.hpp

---
+++ CFunction/con_cfunction.hpp
@@ -0,0 +1,13 @@
+#include "include.h"
+
+namespace CFunction
+{
+
+	void print(std::string value);
+	
+	int strlen(std::string value);
+	double con_pow(std::string A, std::string B);
+	std::string bool_parse(std::string boolean);
+	std::string get_input();
+	std::string do_cfunction(std::string caller, std::string arg1, std::string arg2);
+}
